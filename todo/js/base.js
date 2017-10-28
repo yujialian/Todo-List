6668f68b8d1547777b4bd6180062132100ef48b7
Yujia Lian <yujia.lian001@gmail.com>
2017-10-27 23:51:23 -0400

    Add task details

--- todo/js/base.js
+++ todo/js/base.js
@@ -4,16 +4,27 @@
 ;(function () {
     'use strict';
     var $form_add_task = $('.add-task')
-        , $task_delete
+        , $task_delete_trigger
         , $task_detail
+        , $task_detail_trigger
         , new_task = {}
         , $task_detail = $('.task-detail')
         , $task_detail_mask = $('.task-detail-mask')
         , task_list = []
         ;
 
+
     init();
-    $form_add_task.on('submit', on_add_task_form_submit);
+
+    function init() {
+        task_list = store.get('task_list') || [];
+        if (task_list.length)
+            render_task_list();
+    }
+
+    $form_add_task.on('submit', on_add_task_form_submit)
+    $task_detail_mask.on('click', hide_task_detail)
+
     function on_add_task_form_submit(e) {
         var new_task = {}, $input;
         /*Deactive default task*/
@@ -34,8 +45,8 @@
     /*Looking and listening all the delete button events.*/
     function listen_task_delete()
     {
-        console.log('$task_delete', $task_delete);
-        $task_delete.on('click', function () {
+        //console.log('$task_delete', $task_delete);
+        $task_delete_trigger.on('click', function () {
             var $this = $(this);
             /*Find which event's delete button is been pushed*/
             var $item = $this.parent().parent();
@@ -46,15 +57,6 @@
         })
     }
 
-    function listen_task_detail() {
-        //console.log('$task_detail', $task_detail)
-        $task_detail.on('click', function () {
-            var $this = $(this);
-            var $item = $this.parent().parent();
-            var index = $item.data('index');
-            console.log('index', index);
-        })
-    }
     /*Delete one task*/
     function delete_task(index) {
         /*if no index or index does not exist, return directly*/
@@ -72,11 +74,7 @@
         return true;
     }
 
-    function init() {
-        task_list = store.get('task_list') || [];
-        if (task_list.length)
-            render_task_list();
-    }
+
 
     /*Refresh local storage data and render view*/
     function refresh_task_list() {
@@ -92,21 +90,54 @@
             var $task = render_task_item(task_list[i], i);
             $task_list.append($task);
         }
-        $task_delete = $('.action.delete');
-        $task_detail = $('.action.detail');
+        $task_delete_trigger = $('.action.delete');
+        $task_detail_trigger = $('.action.detail');
         listen_task_delete();
         listen_task_detail();
-        //console.log('1', 1);
     }
 
     function listen_task_detail()
     {
-        $task_detail.on('click',function() {
+        $task_detail_trigger.on('click',function() {
             var $this = $(this);
-            $this.parent().parent();
-
+            var $item = $this.parent().parent();
+            var index = $item.data('index');
+            //console.log('index', index);
+            show_task_detail(index)
         })
     }
+
+    function show_task_detail(index) {
+        render_task_detail();
+        $task_detail.show();
+        $task_detail_mask.show();
+
+    }
+
+    /*render specific task's detail information.*/
+    function render_task_detail(index) {
+        var tpl =         <div>
+        <div class="content"><!-- Task name -->
+        e.g. Don't forget the cannolis!
+        </div><!-- End of task name -->
+        <div><!-- Start of task description -->
+        <div class="desc">
+            <textarea id=""></textarea>
+            </div>
+            </div><!-- End of task description -->
+        <div class="remind"><!-- Task time remainder -->
+        <input type="date">
+            <!--<button type="submit">Submit</button>-->
+            </div><!-- End of task time remainder -->
+        </div>
+    }
+
+    function hide_task_detail() {
+        $task_detail.hide();
+        $task_detail_mask.hide();
+
+    }
+
     /*Render single task templates.*/
     function render_task_item(data, index) {
         if (!data || !index) return;
